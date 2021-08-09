59610faf3b80449a2bdb54ac00b87ef1a3385442
dpepi <dpepi@DESKTOP-37AMI1K>
2021-08-09 19:50:47 +0200

    Add project files.

---
+++ GrupaA/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GrupaA
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int hero_hp = 100;
+			int hero_attack = 20;
+
+			int goblin_hp = 50;
+			int goblin_attack = 20;
+
+			while (hero_hp > 0 && goblin_hp > 0)
+			{
+				goblin_hp = goblin_hp - hero_attack;
+				Console.WriteLine($"Heroj udara za {hero_attack}, goblin sada ima {goblin_hp}.");
+				hero_hp = hero_hp - goblin_attack;
+				Console.WriteLine($"Goblin udara za {goblin_attack}, goblin sada ima {hero_hp}.");
+			}
+		}
+	}
+}
